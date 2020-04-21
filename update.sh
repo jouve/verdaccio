481e0cd6583581dd41810ee9f509232762c07ea1
Cyril Jouve <jv.cyril@gmail.com>
2020-04-21 18:16:59 +0200

    bump base

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+docker run -v $PWD:/usr/src/verdaccio -w /usr/src/verdaccio $(sed -n -e 's/FROM //p' Dockerfile) sh -c "
+apk add --no-cache make nodejs npm python && \
+    npm i
+"
