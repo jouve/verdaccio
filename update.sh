0332ed6d3d43de023cd8618d98ebc97987c26e7d
Cyril Jouve <jv.cyril@gmail.com>
2020-05-17 00:25:02 +0200

    bump

--- update.sh
+++ update.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -x
 
-docker run -v $PWD:/usr/src/verdaccio -w /usr/src/verdaccio $(sed -n -e 's/FROM //p' Dockerfile) sh -c "
-apk add --no-cache make nodejs npm python && \
-    npm i
-"
+docker run -v $PWD:/usr/src/verdaccio -w /usr/src/verdaccio $(sed -n -e 's/FROM //p' Dockerfile) sh -c 'apk add --no-cache make nodejs npm python && npm up'
