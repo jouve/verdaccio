c75fdafe6a578d63d09d774ff2e6d0af95c7f26f
Cyril Jouve <jv.cyril@gmail.com>
2020-05-30 23:10:19 +0200

    improve update

--- update.sh
+++ update.sh
@@ -1,6 +1,22 @@
 #!/bin/bash -x
 
-docker run -v $PWD:/usr/src/verdaccio -w /usr/src/verdaccio $(sed -n -e 's/FROM //p' Dockerfile) sh -c '
+if ! test -w /var/run/docker.sock; then
+  SUDO=sudo
+else
+  SUDO=
+fi
+
+docker volume create apk-cache || true
+docker volume create npm-cache || true
+$SUDO docker run -i -t \
+  -v npm-cache:/root/.npm/_cacache \
+  -v apk-cache:/var/cache/apk \
+  -v $PWD:/usr/src/verdaccio \
+  -w /usr/src/verdaccio \
+  $(sed -n -e 's/FROM //p' Dockerfile) sh -x -c '
+set -e
+apk add --no-cache alpine-conf
+setup-apkcache /var/cache/apk
 apk add --no-cache make nodejs npm python2
 rm -f package-lock.json
 npm i
