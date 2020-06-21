68b94751d65fc1c12774402bdc920995d071043b
Cyril Jouve <jv.cyril@gmail.com>
2020-06-21 12:59:11 +0200

    bump

--- update.sh
+++ update.sh
@@ -11,13 +11,15 @@ docker volume create npm-cache || true
 $SUDO docker run -i -t \
   -v npm-cache:/root/.npm/_cacache \
   -v apk-cache:/var/cache/apk \
-  -v $PWD:/usr/src/verdaccio \
-  -w /usr/src/verdaccio \
+  -v $PWD:/usr/share/verdaccio \
+  -w /srv \
   $(sed -n -e 's/FROM //p' Dockerfile) sh -x -c '
 set -e
 apk add --no-cache alpine-conf
 setup-apkcache /var/cache/apk
 apk add --no-cache make nodejs npm python2
 rm -f package-lock.json
-npm i
+cp /usr/share/verdaccio/package.json .
+npm install verdaccio
+cp package-lock.json /usr/share/verdaccio
 '
